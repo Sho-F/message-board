85513cb83699d5699275f46aec74c8d45f9ebbad
sho.fujimatsu <sfujimatsu1023@gmail.com>
2021-02-02 14:27:27 +0000

    validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,7 +20,7 @@ class MessagesController < ApplicationController
       redirect_to @message
     else
       flash.now[:danger] = "Messageが投稿されませんでした"
-      redner :new
+      render :new
     end
   end
   
